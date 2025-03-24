96f2e73506fa829529ea596e386da28ac2f4c1da
abhinay0804 <nama.abhinay2023@vitstudent.ac.in>
2025-03-24 18:08:04 +0530

    PowerX

--- rewards/scripts/deploy.js
+++ rewards/scripts/deploy.js
@@ -1,18 +1,12 @@
-// scripts/deploy.js
-const hre = require("hardhat");
-
-async function main() {
-    const CarbonCreditNFT = await hre.ethers.getContractFactory("CarbonCreditNFT");
-
-    // Deploy contract (no .deployed() needed in ethers v6)
-    const carbonCreditNFT = await CarbonCreditNFT.deploy();
-
-    console.log("CarbonCreditNFT deployed to:", await carbonCreditNFT.getAddress());
+const hre=require("hardhat");
+async function main(){
+    const CarbonCreditNFT=await hre.ethers.getContractFactory("CarbonCreditNFT");
+    const carbonCreditNFT=await CarbonCreditNFT.deploy();
+    console.log("CarbonCreditNFT deployed to:",await carbonCreditNFT.getAddress());
 }
-
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
+    .then(()=>process.exit(0))
+    .catch((error)=>{
         console.error(error);
         process.exit(1);
     });
