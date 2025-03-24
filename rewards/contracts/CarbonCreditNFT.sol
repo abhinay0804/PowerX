e634e6ad93bbfd283feb400eb77bf748f82ed1d6
abhinay0804 <nama.abhinay2023@vitstudent.ac.in>
2025-03-24 18:04:59 +0530

    PowerX

--- rewards/contracts/CarbonCreditNFT.sol
+++ rewards/contracts/CarbonCreditNFT.sol
@@ -6,31 +6,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract CarbonCreditNFT is ERC721URIStorage, Ownable {
     uint256 public tokenCounter;
     mapping(address => uint256) public userPoints;
-
     event NFTMinted(address recipient, uint256 tokenId, string tokenURI);
-
-    // Pass msg.sender to the Ownable constructor
     constructor() ERC721("Carbon Credit NFT", "CCNFT") Ownable(msg.sender) {
         tokenCounter = 0;
     }
-
-    // Mint NFT when user reaches a certain point threshold
     function mintNFT(address recipient, string memory tokenURI) public onlyOwner returns (uint256) {
         uint256 newTokenId = tokenCounter;
         _safeMint(recipient, newTokenId);
         _setTokenURI(newTokenId, tokenURI);
-        tokenCounter += 1;
-
+        tokenCounter+=1;
         emit NFTMinted(recipient, newTokenId, tokenURI);
         return newTokenId;
     }
-
-    // Update user points
     function updateUserPoints(address user, uint256 points) public onlyOwner {
         userPoints[user] += points;
     }
-
-    // Get user points
     function getUserPoints(address user) public view returns (uint256) {
         return userPoints[user];
     }
